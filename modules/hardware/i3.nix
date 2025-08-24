e181aa9a434c53616624b11d10b50e2349962197
vittorio01 <vittoriopolci@live.com>
2025-08-24 17:21:14 +0200

    addons

--- modules/hardware/i3.nix
+++ modules/hardware/i3.nix
@@ -4,12 +4,12 @@ let
 in {
   config = lib.mkIf (config.hardware.hardware-profile == "I3") {
     boot.kernelPackages = pkgs.linuxPackages_latest;
-    hardware.graphics.enable = true;
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-      modesetting.enable = true;
-    };
+    #hardware.graphics.enable = true;
+    #services.xserver.videoDrivers = [ "nvidia" ];
+    #hardware.nvidia = {
+    #  package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    #  modesetting.enable = true;
+    #};
     hardware.cpu.intel.updateMicrocode = true;
   };  
 }
