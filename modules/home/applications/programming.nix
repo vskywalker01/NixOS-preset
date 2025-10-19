8b8c18901e7916a0f4af4249910e382974b24527
vittorio01 <vittoriopolci@live.com>
2025-10-19 20:11:22 +0200

    added vpn service, system upgrade, vim

--- modules/home/applications/programming.nix
+++ modules/home/applications/programming.nix
@@ -4,6 +4,10 @@ let
   unstable = import flake-inputs.nixpkgs-unstable {
     system = pkgs.system;
   };
+  vimrc = builtins.fetchGit  {
+    url = "https://github.com/danebulat/vim-config.git";
+    rev = "491f6306f46b2eaacb90b5732f121c00e94ec3a2"; 
+  };
 in {
   options.applications = {
     programming = lib.mkOption {
@@ -16,8 +20,6 @@ in {
   config = lib.mkIf (config.applications.programming) {
     programs.vscode = {
       enable = lib.mkDefault true;
-      #enableUpdateCheck = lib.mkDefault false;
-      #enableExtensionUpdateCheck = lib.mkDefault false;
       profiles.default.extensions = with pkgs.vscode-extensions; [
         ms-python.python
         ms-python.vscode-pylance
@@ -37,9 +39,20 @@ in {
       ];
 
     };
-    home.packages = [
-      pkgs.gtkterm
-      pkgs.arduino
+    home.packages = with pkgs; [
+      gtkterm
+      arduino
     ];
+    programs.vim.enable = true;
+    home.file.".vim/bundle/Vundle.vim".source = builtins.fetchGit {
+      url = "https://github.com/VundleVim/Vundle.vim.git";
+      rev = "5548a1a937d4e72606520c7484cd384e6c76b565"; 
+    };
+    home.activation.installVimCustomizations = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ -x "$(command -v vim)" ]; then
+        vim +PluginInstall +qall || true
+      fi
+    '';
+    home.file.".vimrc".source = "${vimrc}/light-ide/vimrc";
   };
 }
