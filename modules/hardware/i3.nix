2b126c491c0c14397dd2ab863ff0af546e963a55
vittorio01 <vittoriopolci@live.com>
2026-01-02 17:37:24 +0100

    bugfix for i3 profile

--- modules/hardware/i3.nix
+++ modules/hardware/i3.nix
@@ -5,25 +5,7 @@ in {
   config = lib.mkIf (config.hardware.hardware-profile == "I3") {
     boot.kernelPackages = pkgs.linuxPackages_latest;
     hardware.graphics.enable = true;
-    #services.xserver.videoDrivers = [ "nvidia" ];
-    #hardware.nvidia = {
-    #  package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-    #  modesetting.enable = true;
-    #};
+    
     hardware.cpu.intel.updateMicrocode = true;
-      boot.kernelModules = [ "nouveau" ];
-    boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_drm" "nvidia_modeset" ];
-    services.xserver = {
-      enable = true;
-      videoDrivers = [ "nouveau" ];
-    };
-
-    hardware.opengl = {
-      enable = true;
-      extraPackages = with pkgs; [
-        mesa
-        mesa.drivers
-      ];
-    };
   };  
 }
