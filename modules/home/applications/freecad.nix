dcc69678d4b0bcf301c97453b18ff77d9acf1a2b
vskywalker01 <vittoriopolci@live.com>
2026-08-03 10:07:08 +0200

    bugfix for freecad

--- modules/home/applications/freecad.nix
+++ modules/home/applications/freecad.nix
@@ -1,6 +1,9 @@
 {config, lib, pkgs,flake-inputs, systemConfig ? {} ,...}:
 {
     config = lib.mkIf(systemConfig.applications.cads.enable){
-        home.packages = with pkgs; [freecad];
+        services.flatpak.packages = [
+            "org.freecad.FreeCAD"
+        ];
+        #home.packages = with pkgs; [freecad];
     };
 }
